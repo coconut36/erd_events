87c504f2aa62500c8a93f85373cf4fe6d303e79f
anonymous anonymous <coconut3691@gmail.com>
2021-12-05 20:22:31 +0000

    Remove controller comments

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,25 +1,20 @@
 class RsvpsController < ApplicationController
   before_action :set_rsvp, only: %i[show edit update destroy]
 
-  # GET /rsvps
   def index
     @q = Rsvp.ransack(params[:q])
     @rsvps = @q.result(distinct: true).includes(:user,
                                                 :event).page(params[:page]).per(10)
   end
 
-  # GET /rsvps/1
   def show; end
 
-  # GET /rsvps/new
   def new
     @rsvp = Rsvp.new
   end
 
-  # GET /rsvps/1/edit
   def edit; end
 
-  # POST /rsvps
   def create
     @rsvp = Rsvp.new(rsvp_params)
 
@@ -35,7 +30,6 @@ class RsvpsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /rsvps/1
   def update
     if @rsvp.update(rsvp_params)
       redirect_to @rsvp, notice: "Rsvp was successfully updated."
@@ -44,7 +38,6 @@ class RsvpsController < ApplicationController
     end
   end
 
-  # DELETE /rsvps/1
   def destroy
     @rsvp.destroy
     message = "Rsvp was successfully deleted."
@@ -57,12 +50,10 @@ class RsvpsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_rsvp
     @rsvp = Rsvp.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def rsvp_params
     params.require(:rsvp).permit(:users_id, :events_id, :response)
   end
